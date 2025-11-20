7bfd4b3d9e5d3f92519b8abd7de50baaf0db29b7
Barthmossr <barthmossr@gmail.com>
2025-11-20 12:45:11 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,30 @@
+import nextJest from 'next/jest'
+
+import type { Config } from 'jest'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+    '!src/lib/utils.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+}
+
+export default createJestConfig(config)
